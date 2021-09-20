26f5ef5da2f5d12e05dba512e7a188ce04c164e2
ibrar5 <83902973+ibrar5@users.noreply.github.com>
2021-09-20 23:22:58 +0600

    Add files via upload

---
+++ ep.js
@@ -0,0 +1,223 @@
+function dub(){
+    if(document.getElementById("inp").checked){
+        document.getElementById("dub").style.display ="block";
+        document.getElementById("sub").style.display="none";
+    }else {
+        document.getElementById("dub").style.display ="none";
+        document.getElementById("sub").style.display="block";
+    }
+}
+function add(){
+    if(screen.width < 576){
+        document.getElementById("sobi").style.display = "none";
+        document.getElementById("cobi").style.display = "inline-block";
+    }
+}
+
+function anime(){
+    var item = document.getElementById("search").value;
+    if(item === "Attack on Titan Season 3 Part 2"){
+        window.open("/epaot3p2.html");
+    }
+    if(item === "attack on titan season 3 part 2"){
+        window.open("/epaot3p2.html");
+    }
+    if(item === "Attack on Titan Season 1"){
+        window.open("/epaot1.html");
+    }
+    if(item === "attack on titan"){
+        window.open("/epaot1.html");
+    }
+    if(item === "attack on titan season 1"){
+        window.open("/epaot1.html");
+    }
+    if(item === "Attack on Titan"){
+        window.open("/epaot1.html");
+    }
+    if(item === "Attack on Titan Season 2"){
+        window.open("/epao2.html");
+    }
+    if(item === "attack on titan season 2"){
+        window.open("/epaot2.html");
+    }
+    if(item === "attack on titan season 3"){
+        window.open("/epaot3.html");
+    }
+    if(item === "Attack on Titan Season 3"){
+        window.open("/epaot3.html");
+    }
+    if(item === "Attack on Titan Season 4"){
+        window.open("/epaot4.html");
+    }
+    if(item === "attack on titan the final season"){
+        window.open("/epaot4.html");
+    }
+    if(item === "attack on titan season 4"){
+        window.open("/epaot4.html");
+    }
+    if(item === "Attack on Titan The Final Season"){
+        window.open("/epaot4.html");
+    }
+    if(item === "code geass"){
+        window.open("/epcg.html");
+    }
+    if(item === "Code Geass"){
+        window.open("/epcg.html");
+    }
+    if(item === "code geass r2"){
+        window.open("/epcg2.html");
+    }
+    if(item === "Code Geass R2"){
+        window.open("/epcg2.html");
+    }
+    if(item === "code geass season 2"){
+        window.open("/epcg2.html");
+    }
+    if(item === "Code Geass Season 2"){
+        window.open("/epcg2.html");
+    }
+    if(item === "death note"){
+        window.open("/epdn.html");
+    }
+    if(item === "Death Note"){
+        window.open("/epdn.html");
+    }
+    if(item === "demon slayer"){
+        window.open("/epds.html");
+    }
+    if(item === "Demon Slayer"){
+        window.open("/epds.html");
+    }
+    if(item === "kimetsu no yaiba"){
+        window.open("/epds.html");
+    }
+    if(item === "Kimetsu no Yaiba"){
+        window.open("/epds.html");
+    }
+    if(item === "Fullmetal Alchemist: Brotherhood"){
+        window.open("/epfmab.html");
+    }
+    if(item === "fullmetal alchemist: brotherhood"){
+        window.open("/epfmab.html");
+    }
+    if(item === "Fullmetal Alchemist Brotherhood"){
+        window.open("/epfmab.html");
+    }
+    if(item === "fullmetal alchemist brotherhood"){
+        window.open("/epfmab.html");
+    }
+    if(item === "hunter x hunter"){
+        window.open("/ephxh.html");
+    }
+    if(item === "Hunter x Hunter"){
+        window.open("/ephxh.html");
+    }
+    if(item === "Jujutsu Kaisen"){
+        window.open("/epjk.html");
+    }
+    if(item === "jujutsu kaisen"){
+        window.open("/epjk.html");
+    }
+    if(item === "My Hero Academia"){
+        window.open("/epmha1.html");
+    }
+    if(item === "My Hero Academia Season 1"){
+        window.open("/epmha1.html");
+    }
+    if(item === "my hero academia"){
+        window.open("/epmha1.html");
+    }
+    if(item === "my hero academia season 1"){
+        window.open("/epmha1.html");
+    }
+    if(item === "My Hero Academia Season 2"){
+        window.open("/epmha2.html");
+    }
+    if(item === "my hero academia season 2"){
+        window.open("/epmha2.html");
+    }
+    if(item === "My Hero Academia Season 3"){
+        window.open("/epmha3.html");
+    }
+    if(item === "my hero academia season 3"){
+        window.open("/epmha3.html");
+    }
+    if(item === "My Hero Academia Season 4"){
+        window.open("/epmha4.html");
+    }
+    if(item === "my hero academia season 4"){
+        window.open("/epmha4.html");
+    }
+    if(item === "My Hero Academia Season 5"){
+        window.open("/epmha5.html");
+    }
+    if(item === "my hero academia season 5"){
+        window.open("/epmha5.html");
+    }
+    if(item === "naruto"){
+        window.open("/epnaruto.html");
+    }
+    if(item === "Naruto"){
+        window.open("/epnaruto.html");
+    }
+    if(item === "naruto shippuden"){
+        window.open("/epns.html");
+    }
+    if(item === "Naruto Shippuden"){
+        window.open("/epns.html");
+    }
+    if(item === "one punch man"){
+        window.open("/epopm.html");
+    }
+    if(item === "One Punch Man"){
+        window.open("/epopm.html");
+    }
+    if(item === "one punch man season 2"){
+        window.open("/epopm2.html");
+    }
+    if(item === "One Punch Man Season 2"){
+        window.open("/epopm2.html");
+    }
+    if(item === "tokyo ghoul"){
+        window.open("/eptg.html");
+    }
+    if(item === "Tokyo Ghoul"){
+        window.open("/eptg.html");
+    }
+    if(item === "tokyo ghoul season 2"){
+        window.open("/eptg.html");
+    }
+    if(item === "Tokyo Ghoul Season 2"){
+        window.open("/eptg.html");
+    }
+    if(item === "tokyo ghoul re"){
+        window.open("/eptgre.html");
+    }
+    if(item === "Tokyo Ghoul Re"){
+        window.open("/eptgre.html");
+    }
+    if(item === "tokyo ghoul: re"){
+        window.open("/eptgre.html");
+    }
+    if(item === "Tokyo Ghoul: Re"){
+        window.open("/eptgre.html");
+    }
+    if(item === "tokyo ghoul re season 2"){
+        window.open("/eptgre2.html");
+    }
+    if(item === "Tokyo Ghoul Re Season 2"){
+        window.open("/eptgre2.html");
+    }
+    if(item === "tokyo ghoul: re season 2"){
+        window.open("/eptgre2.html");
+    }
+    if(item === "Tokyo Ghoul: Re Season 2"){
+        window.open("/eptgre2.html");
+    }
+    if(item === "vinland saga"){
+        window.open("/epvs.html");
+    }
+    if(item === "Vinland Saga"){
+        window.open("/epvs.html");
+    }
+}
